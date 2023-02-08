83d8a0f4cad34a0f532d7d806a340ed14a95d9be
wg <1134614268@qq.com>
2023-02-09 00:36:44 +0800

    u

---
+++ JiangXin/web/src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  {{ imgs }}
+  <div class="p_c_flexbox">
+    <div :key="o" v-for="o in imgs" class="img_box col-3">
+      <div class="img_box2">
+        <div @click="go(Info)" class="imgContainer p_c_test_border">
+          <img :src="'http://127.0.0.1:29090/api/file/download/'+o.imgUrl" class="img imgCover">
+        </div>
+        <div class="imgDesc">
+          <div class="title">
+            {{ o.description }}
+          </div>
+          <div class="star">
+            {{ o.description }}
+          </div>
+          <div class="avatar">
+            {{ o.description }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import {getJson3} from "@/api/http";
+import {getPage} from "@/views/api";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      imgs: [],
+      Info: 123,
+    }
+  },
+  methods: {
+    go(url) {
+      this.$router.push(url)
+    },
+    async getAllTree() {
+      let data = {
+        parentId: 0
+      }
+      let ret = await getJson3(getPage, data)
+      this.imgs =ret.data.records;
+    },
+  },
+  created() {
+    this.getAllTree()
+  }
+}
+</script>
+
+<style scoped lang="less">
+.imgContainer {
+  width: 100%;
+  border: #0b9ef2 solid 1px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+  //align-content: center;
+  //align-items:center;
+  padding-bottom: 75%;
+
+
+}
+
+.img_box{
+  padding: 1rem;
+  border: #b3165f solid 1px;
+
+}
+.img_box2{
+  border: #b098a1 solid 1px;
+  margin: 1rem;
+}
+.img {
+  width: 100%;
+  height: 75%;
+}
+
+.imgCover {
+  object-fit: cover; //fill contain none scale-down
+
+}
+
+</style>
