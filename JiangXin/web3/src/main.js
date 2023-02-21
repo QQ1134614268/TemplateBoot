8b0be7a6a609ee91f15ee333c62a5230de3b6737
wg <1134614268@qq.com>
2023-02-21 22:47:17 +0800

    u

--- JiangXin/web3/src/main.js
+++ JiangXin/web3/src/main.js
@@ -5,8 +5,11 @@ import store from './store'
 import './assets/global.css'
 import '@/assets/g2.css'
 import '@/assets/g3.css'
+import ElementUI from 'element-ui'; //全局引入element
+import 'element-ui/lib/theme-chalk/index.css'; //全局引入element的样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);     //全局注入element
 
 new Vue({
     router,
