dbf55ce84e129a35e9dd171021f59b687fa9d3c2
wg <1134614268@qq.com>
2023-10-20 22:33:19 +0800

    templateVue 独立

--- JiangXin/web3/src/api/img.js
+++
@@ -1,8 +0,0 @@
-import {getJson3} from "@/api/http";
-import {getContent} from "@/api/api";
-
-
-export async function getAllTree2(data) {
-    let ret = await getJson3(getContent, data)
-    return ret.data;
-}
