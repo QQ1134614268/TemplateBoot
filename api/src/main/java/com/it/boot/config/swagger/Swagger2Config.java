3120b63d680ca87d3d986013756b74f1d68c1f92
wg <1134614268@qq.com>
2023-10-30 11:04:15 +0800

    u

--- api/src/main/java/com/it/boot/config/swagger/Swagger2Config.java
+++ api/src/main/java/com/it/boot/config/swagger/Swagger2Config.java
@@ -2,10 +2,8 @@ package com.it.boot.config.swagger;
 
 import com.it.boot.config.Conf;
 import io.swagger.annotations.Api;
-import org.apache.commons.compress.utils.Sets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
@@ -14,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -31,14 +28,9 @@ public class Swagger2Config {
         ApiKey o = new ApiKey("token认证信息", Conf.AUTHORIZATION, ParameterType.HEADER.name());
         List<SecurityScheme> securitySchemes = Collections.singletonList(o);
 
-        // 3. 默认 content-type todo 不起作用? swagger3有默认值? 类似FormData,有自动推断??
-        Set<String> DEFAULT_PRODUCES_AND_CONSUMES = Sets.newHashSet(MediaType.APPLICATION_JSON_VALUE);
-
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
                 // .globalRequestParameters(globalPara) // 全局参数
-                // .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
-                // .produces(DEFAULT_PRODUCES_AND_CONSUMES)
                 .securitySchemes(securitySchemes)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)) // 指定扫描的接口
