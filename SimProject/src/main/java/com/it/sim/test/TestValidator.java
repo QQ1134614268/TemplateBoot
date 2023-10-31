c23107b780f7c1500f2743509518e10dc9fa3b51
wg <1134614268@qq.com>
2023-10-31 09:14:01 +0800

    u

---
+++ SimProject/src/main/java/com/it/sim/test/TestValidator.java
@@ -0,0 +1,78 @@
+package com.it.sim.test;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.*;
+import jakarta.validation.groups.Default;
+import lombok.Data;
+import org.hibernate.validator.HibernateValidator;
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+/**
+ * @date 2023-05-04 18:21
+ * <pre>
+ *  Validator
+ *      validate(T var1, Class<?>... var2);
+ *      validateProperty
+ *      validateValue
+ * </pre>
+ */
+public class TestValidator {
+    @Test
+    public void test1() {
+        Model model = new Model();
+        model.setName("");
+        model.setAge(-1);
+        Set<ConstraintViolation<Model>> validatorResult = ValidatorUtil.validate(model, Model.EditGroup.class, Default.class);
+        System.out.println(validatorResult.stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(",")));
+    }
+}
+
+
+@Data
+class Model {
+
+    @NotNull(message = "id不能为空", groups = EditGroup.class)
+    @Null(message = "id不能有值", groups = AddGroup.class)
+    private Long id;
+
+    @Size(min = 3, max = 20, message = "名称长度不能小于3位且不能大于20位")
+    @NotBlank(message = "名称不能为空")
+    private String name;
+
+    @Max(value = 150, message = "年龄最大150岁")
+    @Min(value = 18, message = "年龄最小18岁")
+    @NotNull(message = "年龄不能为空")
+    private Integer age;
+
+    public interface EditGroup {
+
+    }
+
+    public interface AddGroup {
+
+    }
+}
+
+
+/**
+ * 校验框架工具类
+ */
+final class ValidatorUtil {
+
+    // private static final Validator validator  = Validation.buildDefaultValidatorFactory().getValidator();
+    private static final Validator validator = Validation.byProvider(HibernateValidator.class)
+            .configure()
+            .addProperty("hibernate.validator.fail_fast", "true")
+            .buildValidatorFactory()
+            .getValidator();
+
+    public static <T> Set<ConstraintViolation<T>> validate(T obj, Class<?>... groups) {
+        return validator.validate(obj, groups);
+    }
+}
