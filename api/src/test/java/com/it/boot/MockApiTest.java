449329dcc95a391a7c7a1cd75496791c2f6d93ea
wg <1134614268@qq.com>
2023-12-09 23:23:23 +0800

    u

--- api/src/test/java/com/it/boot/MockApiTest.java
+++ api/src/test/java/com/it/boot/MockApiTest.java
@@ -1,6 +1,7 @@
 package com.it.boot;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -16,14 +17,16 @@ public class MockApiTest {
     private WebApplicationContext wac;
     private MockMvc mockMvc;
 
-    @Before
+    @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
-    @org.junit.Test
+    @Test
     public void test01() throws Exception { // springboot 带有的测试框架 ,, 上面代码 会在api测试开始前,构建测试需要的后台服务
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/all/hi").param("count", "110").param("message", "zero")
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/all/hi")
+                .param("count", "110")
+                .param("message", "zero")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(MockMvcResultMatchers.status().isOk());
         // MockMvcRequestBuilders.get("/api/all/hi") 发起一个请求,,
         // "/api/all/hi",,代表向本服务发起一个url为 localhost:该应用配置的端口/api/all/hi 的请求
