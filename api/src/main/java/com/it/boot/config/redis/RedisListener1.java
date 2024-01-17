feef9f3f0da75866fed7e37a53b36ea63b37abd9
wg <1134614268@qq.com>
2024-01-17 17:33:40 +0800

    u

--- api/src/main/java/com/it/boot/config/redis/RedisListener1.java
+++ api/src/main/java/com/it/boot/config/redis/RedisListener1.java
@@ -1,12 +1,12 @@
 package com.it.boot.config.redis;
 
+import com.alibaba.fastjson2.JSON;
 import com.it.boot.entity.dto.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
 
@@ -18,14 +18,9 @@ public class RedisListener1 implements MessageListener {
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-
-        // 接收的topic
-        log.info("channel:" + new String(pattern));
-
-        //序列化对象（特别注意：发布的时候需要设置序列化；订阅方也需要设置序列化）
+        // 序列化对象（特别注意：发布的时候需要设置序列化；订阅方也需要设置序列化）
         MessageDto messageDto = (MessageDto) redisTemplate.getValueSerializer().deserialize(message.getBody());
-        Assert.notNull(messageDto, "not null");
-        log.info("{}, {} ", messageDto.getData(), messageDto.getContent());
+        log.info("收到发布消息-- topic: {}, data: {}", redisTemplate.getKeySerializer()
+                .deserialize(pattern), JSON.toJSONString(messageDto));
     }
 }
-
