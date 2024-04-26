24183c6b3ddec97e25ba89597476b632afef74d1
wg <1134614268@qq.com>
2024-04-26 09:54:48 +0800

    u

--- cloud/cloud-order/src/main/java/com/cloud/order/OrderApp.java
+++ cloud/cloud-order/src/main/java/com/cloud/order/OrderApp.java
@@ -7,16 +7,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class OrderApp {
-	public static void main(String[] args) {
-		SpringApplication.run(OrderApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApp.class, args);
+    }
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- cloud/cloud-user/src/main/java/com/cloud/user/UserApp.java
+++ cloud/cloud-user/src/main/java/com/cloud/user/UserApp.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
-// 开启注册中心server
 @EnableEurekaClient
+@SpringBootApplication
 public class UserApp {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApp.class, args);
