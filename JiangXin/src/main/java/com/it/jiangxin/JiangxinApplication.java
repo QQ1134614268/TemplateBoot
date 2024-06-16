a5d2b28c088a09bb75fb1b44dbf3fd06ccc4c947
wg <1134614268@qq.com>
2024-06-16 22:13:19 +0800

    u

--- JiangXin/src/main/java/com/it/jiangxin/JiangxinApplication.java
+++ JiangXin/src/main/java/com/it/jiangxin/JiangxinApplication.java
@@ -2,15 +2,14 @@ package com.it.jiangxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class JiangxinApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JiangxinApplication.class, args);
-        // SpringApplication app = new SpringApplication(WebApplication.class);
-        // Environment env = app.run(args).getEnvironment();
-        // System.out.println(env.getProperty("server.port"));
-        System.out.println("http://127.0.0.1:29090/doc.html");
+        SpringApplication app = new SpringApplication(JiangxinApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        System.out.println(String.format("http://127.0.0.1:%s/doc.html", env.getProperty("server.port")));
     }
 }
