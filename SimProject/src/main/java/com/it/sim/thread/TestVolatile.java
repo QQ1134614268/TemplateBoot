6ceea2e8250e753211e1e0539fbbf6a793338179
wg <1134614268@qq.com>
2024-06-24 15:54:07 +0800

    u

--- SimProject/src/main/java/com/it/sim/thread/TestVolatile.java
+++ SimProject/src/main/java/com/it/sim/thread/TestVolatile.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CountDownLatch;
 
 @Slf4j
-public class VolatileTest {
+public class TestVolatile {
 
     // volatile 关键字
     public static volatile int race = 0;
