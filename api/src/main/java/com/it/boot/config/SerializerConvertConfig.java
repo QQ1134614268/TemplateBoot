34557b19a0e8d81e54261527a29fcaf69e0bf6f6
wg <1134614268@qq.com>
2024-07-19 00:17:10 +0800

    u

--- api/src/main/java/com/it/boot/config/SerializerConvertConfig.java
+++ api/src/main/java/com/it/boot/config/SerializerConvertConfig.java
@@ -3,6 +3,7 @@ package com.it.boot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,24 +18,37 @@ public class SerializerConvertConfig {
 
     @Bean
     public Converter<String, Date> dateConverter() {
-        return source -> {
-            SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
-            try {
-                return sdf.parse(source);
-            } catch (ParseException e) {
-                throw new IllegalArgumentException("Invalid date format. Should be '" + DATE_TIME_FORMAT + "'", e);
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(@NonNull String source) {
+                SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
+                try {
+                    return sdf.parse(source);
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException(String.format("格式不正确: %s", DATE_TIME_FORMAT), e);
+                }
             }
         };
     }
 
     @Bean
     public Converter<String, LocalDate> localDateConverter() {
-        return LocalDate::parse;
+        return new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(@NonNull String source) {
+                return LocalDate.parse(source);
+            }
+        };
     }
 
     @Bean
     public Converter<String, LocalDateTime> localDateTimeConverter() {
-        return LocalDateTime::parse;
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(@NonNull String source) {
+                return LocalDateTime.parse(source);
+            }
+        };
     }
 
 }
