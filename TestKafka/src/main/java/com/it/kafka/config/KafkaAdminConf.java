11805e4239327a580dedd59282e21d20883090a4
wg <1134614268@qq.com>
2024-07-21 00:13:52 +0800

    u

--- TestKafka/src/main/java/com/it/kafka/config/KafkaAdminConf.java
+++ TestKafka/src/main/java/com/it/kafka/config/KafkaAdminConf.java
@@ -1,23 +1,18 @@
 package com.it.kafka.config;
 
 import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
+import java.util.Map;
 
 @Component
 public class KafkaAdminConf {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
     @Bean
-    AdminClient adminClient() {
-        Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    AdminClient adminClient(KafkaProperties properties) {
+        Map<String, Object> props = properties.buildConsumerProperties();
         return AdminClient.create(props);
     }
 }
