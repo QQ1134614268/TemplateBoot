5f9a37a242dc7526add1b8f5093a08cc258c7e86
wg <1134614268@qq.com>
2024-07-26 09:13:44 +0800

    u

--- TestKafka/src/main/java/com/it/kafka/stream/KafkaStreamsConfig.java
+++ TestKafka/src/main/java/com/it/kafka/stream/KafkaStreamsConfig.java
@@ -1,76 +1,114 @@
 package com.it.kafka.stream;
 
+import com.alibaba.fastjson2.JSON;
+import com.it.kafka.config.ConstConf;
+import com.it.kafka.entity.KafkaUser;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.UnsatisfiedDependencyException;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
-import org.springframework.kafka.config.StreamsBuilderFactoryBean;
-import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.Properties;
 
 @Slf4j
-@EnableKafkaStreams
-@Configuration
+@Component
 public class KafkaStreamsConfig {
-    @Resource
-   private KafkaProperties properties;
-    /**
-     * @see KafkaStreamsDefaultConfiguration
-     */
+    // 在上面的示例中，我们通过将KafkaStreams实例声明为一个 @Bean 方法的返回值来实现了这一点。
+    // 这样，当Spring容器启动时，它就会自动调用这个方法并启动Kafka Streams实例。
+    // 同时，Spring容器还会负责在应用程序关闭时优雅地关闭Kafka Streams实例。
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${app.id:id_001}") // 假设你在application.properties或application.yml中定义了app.id
+    private String applicationId;
+
     @Bean
-    public KafkaStreamsConfiguration defaultKafkaStreamsConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "applicationId");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_001");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, JsonDeserializer.class.getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonDeserializer.class.getName());
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-        return new KafkaStreamsConfiguration(props);
+    public KafkaStreams testKafkaStreams() {
+        StreamsBuilder kStreamBuilder = new StreamsBuilder();
+        // 构建Kafka Streams拓扑
+        KStream<String, String> kStream = kStreamBuilder
+                .stream(ConstConf.USER_TOPIC_OBJECT, Consumed.with(Serdes.String(), Serdes.String()))
+                // .map((k, v) -> (k,v))
+                .filter((key, value) -> {
+                    KafkaUser user = JSON.parseObject(value, KafkaUser.class);
+                    return user.getAge() % 2 == 0;
+                });
+        kStream.to(ConstConf.STREAM_OUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+
+        // 构建KafkaStreams实例的配置属性
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // 这里可以添加更多的配置属性，如处理保证、状态存储配置等
+
+        // 创建KafkaStreams实例
+        KafkaStreams streams = new KafkaStreams(kStreamBuilder.build(), props);
+
+        // 设置状态监听器（可选）
+        streams.setStateListener((newState, oldState) -> {
+            if (newState == KafkaStreams.State.RUNNING) {
+                System.out.println("Kafka Streams is running.");
+            }
+        });
+
+        // 启动Kafka Streams
+        streams.start();
+
+        // 返回KafkaStreams实例以便Spring容器管理其生命周期
+        return streams;
     }
 
-    // @Bean
-    public StreamsBuilderFactoryBean streamBuild1(KafkaStreamsConfiguration defaultKafkaStreamsConfig) {
-        return new StreamsBuilderFactoryBean(defaultKafkaStreamsConfig);
-    }
-    public static final String DEFAULT_STREAMS_CONFIG_BEAN_NAME = "defaultKafkaStreamsConfig";
+    @Bean
+    public KafkaStreams testKafkaStreamsV2() {
+        StreamsBuilder kStreamBuilder = new StreamsBuilder();
+        // 构建Kafka Streams拓扑
+        KStream<String, String> source = kStreamBuilder.stream(ConstConf.USER_TOPIC_OBJECT, Consumed.with(Serdes.String(), Serdes.String()));
+        // messageStream.map()
+        // messageStream.flatMap()
+        // messageStream.flatMapValues()
+        // messageStream.filter()
+        // messageStream.groupBy()
+        // messageStream.foreach();
+        // messageStream.filterNot()
+        // messageStream.flatTransform()
+        // messageStream.groupBy()
+        // messageStream.groupByKey()
+        // messageStream.join();
+        // messageStream.leftJoin()
+        // messageStream.mapValues()
+        // messageStream.merge();
+        // messageStream.peek();
+        // messageStream.process();
+        // messageStream.repartition();
+        // messageStream.split()
+        // messageStream.to();
+        // messageStream.toTable();
 
+        // KTable<String, Long> wordCounts = messageStream
+        //         .mapValues((ValueMapper<String, String>) String::toLowerCase)
+        //         .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+        //         .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
+        //         .count();
+        // wordCounts.toStream().to("output-topic");
 
-    /**
-     * The bean name for auto-configured default {@link StreamsBuilderFactoryBean}.
-     */
-    public static final String DEFAULT_STREAMS_BUILDER_BEAN_NAME = "defaultKafkaStreamsBuilder";
+        // 构建KafkaStreams实例的配置属性
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 
-    // @Bean
-    public StreamsBuilderFactoryBean streamBuild3(@Qualifier(DEFAULT_STREAMS_CONFIG_BEAN_NAME) ObjectProvider<KafkaStreamsConfiguration> streamsConfigProvider, ObjectProvider<StreamsBuilderFactoryBeanConfigurer> configurerProvider) {
+        KafkaStreams streams = new KafkaStreams(kStreamBuilder.build(), props);
 
-        KafkaStreamsConfiguration streamsConfig = streamsConfigProvider.getIfAvailable();
-        if (streamsConfig != null) {
-            StreamsBuilderFactoryBean fb = new StreamsBuilderFactoryBean(streamsConfig);
-            Set<StreamsBuilderFactoryBeanConfigurer> configuredBy = new HashSet<>();
-            configurerProvider.orderedStream().forEach(configurer -> {
-                configurer.configure(fb);
-                configuredBy.add(configurer);
-            });
-            return fb;
-        } else {
-            throw new UnsatisfiedDependencyException(KafkaStreamsDefaultConfiguration.class.getName(), DEFAULT_STREAMS_BUILDER_BEAN_NAME, "streamsConfig", "There is no '" + DEFAULT_STREAMS_CONFIG_BEAN_NAME + "' " + KafkaStreamsConfiguration.class.getName() + " bean in the application context.\n" + "Consider declaring one or don't use @EnableKafkaStreams.");
-        }
+        // 启动Kafka Streams
+        streams.start();
+
+        // 返回KafkaStreams实例以便Spring容器管理其生命周期
+        return streams;
     }
 }
