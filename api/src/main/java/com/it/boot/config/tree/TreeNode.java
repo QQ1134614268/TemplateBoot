699644ac7fe43f0044321ac923bf00d5d3268cdf
wg <1134614268@qq.com>
2024-08-11 22:01:33 +0800

    u

--- api/src/main/java/com/it/boot/config/tree/TreeNode.java
+++ api/src/main/java/com/it/boot/config/tree/TreeNode.java
@@ -1,10 +1,6 @@
 package com.it.boot.config.tree;
 
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -43,37 +39,4 @@ public interface TreeNode {
         return ret;
     }
 
-    static void main(String[] args) {
-        @Data
-        @NoArgsConstructor
-        class Test implements TreeNode {
-            Long id;
-            Long parentId;
-            List<Test> children = new ArrayList<>();
-
-            private Test(Long id, Long parentId) {
-                this.id = id;
-                this.parentId = parentId;
-            }
-        }
-        @EqualsAndHashCode(callSuper = true)
-        @Data
-        @NoArgsConstructor
-        class TestV2 extends Test {
-            Long id;
-            Long parentId;
-            List<Test> children = new ArrayList<>();
-        }
-        Test test1 = new Test(1L, null);
-        Test test2 = new Test(2L, null);
-        Test test3 = new Test(3L, 2L);
-        Test test4 = new Test(4L, 2L);
-        ArrayList<Test> list = new ArrayList<>();
-        list.add(test1);
-        list.add(test2);
-        list.add(test3);
-        list.add(test4);
-        List<Test> ret = getTree(list);
-        ret.forEach(System.out::println);
-    }
 }
