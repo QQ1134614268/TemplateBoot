a0aeb5001886218f6b08cc842bd1cfacc929017d
wg <1134614268@qq.com>
2024-09-30 23:38:58 +0800

    u

--- JiangXin/src/main/java/com/it/jiangxin/controller/LogController.java
+++ JiangXin/src/main/java/com/it/jiangxin/controller/LogController.java
@@ -10,9 +10,7 @@ import com.it.jiangxin.util.BoolUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -26,8 +24,8 @@ public class LogController {
     private LogService logService;
 
     @Operation(summary = "新增")
-    @GetMapping("/create")
-    public ApiResult<Boolean> create(LogEntity entity) {
+    @PostMapping("/create")
+    public ApiResult<Boolean> create(@RequestBody LogEntity entity) {
         return ApiResult.success(logService.save(entity));
     }
 
