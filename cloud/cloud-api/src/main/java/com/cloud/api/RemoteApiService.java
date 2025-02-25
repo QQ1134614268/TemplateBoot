67eb00e0ce22dc6f98a93a568059628ac4f05f22
wg <1134614268@qq.com>
2025-02-25 11:53:41 +0800

    u

--- cloud/cloud-api/src/main/java/com/cloud/api/RemoteApiService.java
+++ cloud/cloud-api/src/main/java/com/cloud/api/RemoteApiService.java
@@ -3,11 +3,11 @@ package com.cloud.api;
 import com.cloud.base.ApiResult;
 import com.cloud.base.StockDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Component
+// Feign 客户端接口是由 Spring Cloud OpenFeign 动态代理生成的，不需要显式添加 @Component
+// fallbackFactory 用于指定降级处理的工厂类，实现 FallbackFactory 接口，并且是一个 Spring Bean。
 @FeignClient(name = "cloud-user", fallbackFactory = RemoteApiServiceFallbackFactory.class) // 使用服务名称
 public interface RemoteApiService {
     @PostMapping(value = "/stock/decrease")
