d344f56525f1f15aab2974b835c2b8ba0eb4219a
wg <1134614268@qq.com>
2025-03-14 00:02:32 +0800

    u

--- cloud/cloud-eureka/src/main/java/com/cloud/eureka/EurekaServerApp.java
+++ cloud/cloud-eureka/src/main/java/com/cloud/eureka/EurekaServerApp.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer// 开启注册中心server
 @SpringBootApplication
-public class CloudEurekaServerApp {
+public class EurekaServerApp {
 	public static void main(String[] args) {
-		SpringApplication.run(CloudEurekaServerApp.class, args);
+		SpringApplication.run(EurekaServerApp.class, args);
 	}
 }
