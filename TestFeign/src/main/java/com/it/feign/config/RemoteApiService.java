c190dbf64ffe760622ade3a5af49932f530bf0f5
wg <1134614268@qq.com>
2025-03-14 20:57:53 +0800

    u

--- TestFeign/src/main/java/com/it/feign/config/RemoteApiService.java
+++ TestFeign/src/main/java/com/it/feign/config/RemoteApiService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "remoteApiService", url = "${TestFeign.sumService.url}",configuration = {FeignTextPlainConf.class},  fallbackFactory = RemoteApiServiceFallbackFactory.class)
+@FeignClient(name = "RemoteApiServiceClient", url = "${TestFeign.sumService.url}", configuration = {FeignTextPlainConf.class}, fallbackFactory = RemoteApiServiceFallbackFactory.class)
 public interface RemoteApiService {
 
     @GetMapping(value = "${TestFeign.sumService.sumPath}") // 支持${} 表达式, 不支持#{} 表达式
--- TestFeign/src/main/java/com/it/feign/config/RemoteHttpsService.java
+++ TestFeign/src/main/java/com/it/feign/config/RemoteHttpsService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(name = "remoteHttpsService", url = "https://localhost:443", configuration = {FeignIgnoreHttpsConf.class}, fallbackFactory = RemoteHttpsServiceFallbackFactory.class)
+@FeignClient(name = "RemoteHttpsServiceClient", url = "https://localhost:443", configuration = {FeignIgnoreHttpsConf.class}, fallbackFactory = RemoteHttpsServiceFallbackFactory.class)
 public interface RemoteHttpsService {
     @GetMapping(value = "/api/HelloController/helloHttps")
     ApiResult<String> helloHttps();
