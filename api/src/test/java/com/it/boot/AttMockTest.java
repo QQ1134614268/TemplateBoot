c901f71e85053efe816581994846301f8ede161a
wg <1134614268@qq.com>
2025-03-15 11:27:06 +0800

    Mock测试

--- api/src/test/java/com/it/boot/AttMockTest.java
+++ api/src/test/java/com/it/boot/AttMockTest.java
@@ -9,11 +9,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT) // 使用 LenientStubs 模式, 放宽限制
 public class AttMockTest {
 
     @Mock
