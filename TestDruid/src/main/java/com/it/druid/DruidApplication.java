942576253a20baa25c984d1649023276c6a0c1c3
wg <1134614268@qq.com>
2025-06-24 12:35:09 +0800

    u

--- TestDruid/src/main/java/com/it/druid/DruidApplication.java
+++ TestDruid/src/main/java/com/it/druid/DruidApplication.java
@@ -7,12 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
-@MapperScan({"com.gitee.sunchenbin.mybatis.actable.dao.*", "com.it.druid.mapper"})
-@ComponentScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.manager.*"}, basePackageClasses = DruidApplication.class)
 @SpringBootApplication
 public class DruidApplication {
     public static void main(String[] args) {
-        System.out.println("http://127.0.0.1:8080/druid");
+        System.out.println("http://127.0.0.1:20624/druid");
         SpringApplication.run(DruidApplication.class, args);
     }
 }
